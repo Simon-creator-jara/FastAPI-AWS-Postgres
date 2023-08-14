f3982452d346cc352744b3a9db3064f9af854009
Simon-creator-jara <70153636+Simon-creator-jara@users.noreply.github.com>
2023-08-14 06:59:01 -0500

    Add Cors

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
 from resources.routes import api_router
 from db import database
+from fastapi.middleware.cors import CORSMiddleware
+
+origins=[
+    "http://localhost",
+    "http://localhost:8000"
+]
 
 app = FastAPI()
 app.include_router(api_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.on_event("startup")
 async def startup():
